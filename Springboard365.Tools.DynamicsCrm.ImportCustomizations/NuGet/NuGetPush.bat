34407373bed0ba9029fbafa67ca7aaeb9c85e75f
Davesmall28 <dave@dsmall.co.uk>
2017-05-21 20:05:14 +0100

    Add beta1 to build script.

--- Springboard365.Tools.DynamicsCrm.ImportCustomizations/NuGet/NuGetPush.bat
+++ Springboard365.Tools.DynamicsCrm.ImportCustomizations/NuGet/NuGetPush.bat
@@ -1,4 +1,4 @@
-NuGet.exe pack ../Springboard365.Tools.DynamicsCrm.ImportCustomizations.csproj -Build -Symbols
+NuGet.exe pack ../Springboard365.Tools.DynamicsCrm.ImportCustomizations.csproj -Build -Symbols -Version 1.1.0-beta1
 
 NuGet.exe push *.nupkg
 
