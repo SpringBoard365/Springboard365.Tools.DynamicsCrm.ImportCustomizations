0bd10045e1e0eb7c87202636a3c415dc16ce57ae
Davesmall28 <dave@dsmall.co.uk>
2017-05-21 20:01:48 +0100

    Update build configuration to AnyCPU instead of x64 only.

--- Springboard365.Tools.DynamicsCrm.ImportCustomizations/NuGet/NuGetPush.bat
+++ Springboard365.Tools.DynamicsCrm.ImportCustomizations/NuGet/NuGetPush.bat
@@ -1,4 +1,4 @@
-NuGet.exe pack ../Springboard365.Tools.DynamicsCrm.ImportCustomizations.csproj -Build -Symbols -Version 1.0.0
+NuGet.exe pack ../Springboard365.Tools.DynamicsCrm.ImportCustomizations.csproj -Build -Symbols
 
 NuGet.exe push *.nupkg
 
