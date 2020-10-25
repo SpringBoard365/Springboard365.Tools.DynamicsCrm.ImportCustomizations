8c8d5f518457a8c96a7d3a3af6bc6561b63b3cd3
David Small <dave@dsmall.co.uk>
2020-10-25 14:39:27 +0000

    feat: Update build script.

--- Build/NuGetPush.bat
+++ Build/NuGetPush.bat
@@ -1,13 +1,16 @@
+@echo off
 SET packageVersion=2.0.0-alpha01
 
 SET configuration=Release
-SET id="Springboard365.Tools.DynamicsCrm.ImportCustomizations";
-SET author="Springboard 365 Ltd";
-SET repo="https://github.com/SpringBoard365/Springboard365.Tools.DynamicsCrm.ImportCustomizations";
-SET description="Import customizations application to allow for automation of Power Platform Application Lifecycle Management.";
-SET tags="Springboard365BuildTool PowerPlatformBuildTool Dynamics365BuildTool DynamicsCrmBuildTool XrmBuildTool";
+SET id=Springboard365.Tools.DynamicsCrm.ImportCustomizations
+SET author="Springboard 365 Ltd"
+SET repo="https://github.com/SpringBoard365/Springboard365.Tools.DynamicsCrm.ImportCustomizations"
+SET description="Import customizations application to allow for automation of Power Platform Application Lifecycle Management."
+SET tags="Springboard365BuildTool PowerPlatformBuildTool Dynamics365BuildTool DynamicsCrmBuildTool XrmBuildTool"
 
-dotnet build ../src/CommandLine.Core.csproj -c  %configuration% -p:Version=%packageVersion% -f net462 --nologo
+dotnet restore ../%id%.sln
+
+dotnet build ../%id%.sln -c  %configuration% -p:Version=%packageVersion% -f net462 --nologo
 
 pause
 
